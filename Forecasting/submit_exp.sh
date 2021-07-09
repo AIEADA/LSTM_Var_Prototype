3909236a8ecbd289e08704598f2de1c4edf1eed8
Romit <rmaulik@anl.gov>
2021-07-09 18:24:12 +0000

    Added example submission script ThetaGPU

---
+++ Forecasting/submit_exp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#COBALT -A datascience
+#COBALT -n 1
+#COBALT -q full-node
+#COBALT -t 60
+
+START1="$(date +%s)"
+
+source /lus/theta-fs0/software/thetagpu/conda/2021-06-28/mconda3/setup.sh
+conda activate /lus/eagle/projects/datascience/rmaulik/LSTM_Var_Prototype/AIAEDA
+
+# For running experiment
+python source/main.py
+python source/comparisons.py
