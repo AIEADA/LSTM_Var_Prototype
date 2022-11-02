783bd2e9a732f4cffc77461d0518b7304c3588c1
Bethany Lusch <bethanyl@anl.gov>
2022-11-02 16:08:40 -0500

    test new autoencoder speed

--- Autoencoder/exp2.py
+++ Autoencoder/exp2.py
@@ -1,3 +1,3 @@
 from AE_LSTM_Physical_Pipelined_CNN import main
 
-main(folder_name="exp2", model_name='autoencoder1', num_epochs=5)
+main(folder_name="exp2", model_name='autoencoder2', num_epochs=5)
