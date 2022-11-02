dd12aefd74d8f78226667d7f790283de31ccdd72
Bethany Lusch <bethanyl@anl.gov>
2022-11-02 14:45:11 -0500

    increase epochs

--- Autoencoder/AE_LSTM_Physical_Pipelined_CNN.py
+++ Autoencoder/AE_LSTM_Physical_Pipelined_CNN.py
@@ -156,7 +156,7 @@ def main():
     #model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),loss='mean_squared_error',loss_weights=[1,1])
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),loss='mean_squared_error')
     model.summary()
-    history = model.fit(train_dataset,epochs=5,callbacks=[checkpointing], verbose=2)
+    history = model.fit(train_dataset,epochs=50,callbacks=[checkpointing], verbose=2)
     with open('history.pkl', 'wb') as file_pi:
         pickle.dump(history.history, file_pi)
 
